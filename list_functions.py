cb7873aa5b4fd4cf3a070e9dcd5c6b82d003e81f
Rohit <rohit.pj95@gmail.com>
2023-12-31 01:03:41 +0530

    changes to list functions file

--- list_functions.py
+++ list_functions.py
@@ -43,17 +43,36 @@ print(l3)
 # syntax: range(<start_number>, <end_number>). <start_number> is an optional field. By default <start_numer> is 0.
 # range(<start_number>, <end_number>) will print values starting (<start_number>, ...., <end_number-1>)
 
-print(range(4)) # is same as # print(range(0, 4))
+range(4) # is same as # print(range(0, 4))
 # this will print values 0, 1, 2, 3.
 
-print(range(5))
+range(5)
 # this will print values 0, 1, 2, 3, 4
 
+range(1, 5)
+# this will print values from 1 to 4 i.e. 1,2,3,4
+
+range(0,7,2)
+# this will print numners in difference of 2 starting with 0 until 6 i.e. 0,2,
+
+# To print elements from range function
+
+for number in range(0,7,2):
+    print(number)
+
 ######################################### 6. accessing element of the list ###############################################################
+
+# To access elements of a list we generally use range function
+# Syntax: <name_of_the_list>[<index_number_at_which element is to be accessed]
+
+l3 = [48, 59, 39, 15]
+#print(l3[0]) # This will print element at index 0 i.e. '48'
+print(l3[1])
 
 # Example: Print values at index 0, 1, 2
 l3 = [48, 59, 39, 15]
 
 for element in range(3): # This will create a list [0, 1, 2] for looping
     print(l3[element])
+   
     
