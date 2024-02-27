6b4d7208f60dce3fca7e70b8de8b4cf16708ffdb
Rohit <rohit.pj95@gmail.com>
2024-02-27 11:42:18 -0800

    script

--- data_analysis/miners.py
+++ data_analysis/miners.py
@@ -15,8 +15,17 @@ for column in data_sheet.iter_cols():
             if i > 0 and cell.value == 'Miner':
               total_miners_sold.update({data_sheet.cell(row=i+1, column=6).value:data_sheet.cell(row=i+1, column=16).value})
 
+sum = 0
+
+for each_value in list(total_miners_sold.values()):
+    print(each_value)
+    if each_value == 'None' or each_value == '1.08 BTC':
+        continue
+    else:
+        sum = sum + each_value
+
 total_number_miners_sold = len(list(total_miners_sold.keys()))
-total_paybles_from_miners = sum(list(total_miners_sold.values()))
+total_paybles_from_miners = sum
 
 print(f'Total number of miners sold are {total_number_miners_sold}')
 print(f'Total paybles received from miners are {total_paybles_from_miners}')
