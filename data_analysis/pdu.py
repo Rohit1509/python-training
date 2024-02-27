6cd89a1d50dff1cf7a784ff9aad6446e22e019d6
Rohit <rohit.pj95@gmail.com>
2024-02-27 12:07:41 -0800

    pdu

--- data_analysis/pdu.py
+++ data_analysis/pdu.py
@@ -15,8 +15,17 @@ for column in data_sheet.iter_cols():
             if i > 0 and cell.value == 'Transformer':
               total_pdus_sold.update({data_sheet.cell(row=i+1, column=6).value:data_sheet.cell(row=i+1, column=16).value})
 
+sum = 0
+
+for each_value in list(total_pdus_sold.values()):
+    print(each_value)
+    if each_value == 'None' or each_value == '1.08 BTC':
+        continue
+    else:
+        sum = sum + each_value
+
 total_number_pdus_sold = len(list(total_pdus_sold.keys()))
-total_paybles_from_pdus = sum(list(total_pdus_sold.values()))
+total_paybles_from_pdus = sum
 
 print(f'Total number of pdus sold are {total_number_pdus_sold}')
 print(f'Total paybles received from pdus are {total_paybles_from_pdus}')
